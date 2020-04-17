90c785060e6819a63ae631cd62f9d75656538a82
wuxjian <wu_xjian@163.com>
2020-04-17 08:29:15 +0800

    部分修改

--- src/app/pages/manage/sidebar/sidebar.component.ts
+++ src/app/pages/manage/sidebar/sidebar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.less']
 })
 export class SidebarComponent implements OnInit {
+  open = true;
 
   constructor() { }
 
